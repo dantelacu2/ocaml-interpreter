96f2791e49d1a6894d4864b0a724d526977b4736
dantelacu2 <55060638+dantelacu2@users.noreply.github.com>
2020-05-05 13:01:00 -0700

    checkpoint 1

--- expr.ml
+++ expr.ml
@@ -58,14 +58,17 @@ let same_vars : varidset -> varidset -> bool =
    Generate a set of variable names from a list of strings (for
    testing purposes) *)
 let vars_of_list : string list -> varidset =
-  SS.of_list ;;
+  SS.of_list ;; 
   
 (* free_vars : expr -> varidset
    Return a set of the variable names that are free in expression
    exp *)
 let free_vars (exp : expr) : varidset =
-  failwith "free_vars not implemented" ;;
-  
+  (* match exp with 
+  | Var x -> vars_of_list [x]
+  | _ ->  ;; *)
+  failwith "free_vars";;
+
 (* new_varname : unit -> varid
    Return a fresh variable, constructed with a running counter a la
    gensym. Assumes no variable names use the prefix "var". (Otherwise,
@@ -90,13 +93,65 @@ let subst (var_name : varid) (repl : expr) (exp : expr) : expr =
   String representations of expressions
  *)
    
-    
+let binop_to_concrete_string (bin: binop) : string =
+  match bin with
+  | Plus -> "+"
+  | Minus -> "-"
+  | Times -> "*"
+  | Equals -> "="
+  | LessThan -> "<"
+  ;;
 (* exp_to_concrete_string : expr -> string
    Returns a concrete syntax string representation of the expr *)
-let exp_to_concrete_string (exp : expr) : string =
-  failwith "exp_to_concrete_string not implemented" ;;
+let rec exp_to_concrete_string (exp : expr) : string =
+  match exp with
+  | Var x -> x
+  | Num x -> string_of_int x
+  | Bool x -> string_of_bool x
+  | Unop (_, y) -> "-" ^ (exp_to_concrete_string y)
+  | Binop (x, y, z) -> (exp_to_concrete_string y) ^ 
+                       (binop_to_concrete_string x) ^ 
+                       (exp_to_concrete_string z)
+  | Conditional (x, y, z) -> "if (" ^ (exp_to_concrete_string x) ^ ") then " 
+                                    ^ (exp_to_concrete_string y) ^ "else " 
+                                    ^ (exp_to_concrete_string z)
+  | Fun (x, y) -> "function (" ^ x ^ ") -> " 
+                               ^ (exp_to_concrete_string y) 
+  | Let (x, y, z) -> "let " ^ x ^ " = " ^ (exp_to_concrete_string y) ^ " in " ^ (exp_to_concrete_string z) 
+  | Letrec (x, y, z) -> "let rec " ^ x ^ " = " ^ (exp_to_concrete_string y) ^ " in " ^ (exp_to_concrete_string z) 
+  | Raise -> raise Exit
+  | Unassigned -> "Unassigned"
+  | App (x, y) -> (exp_to_concrete_string x) ^ " applied to " ^ (exp_to_concrete_string y) ;;
 
 (* exp_to_abstract_string : expr -> string
    Returns a string representation of the abstract syntax of the expr *)
-let exp_to_abstract_string (exp : expr) : string =
-  failwith "exp_to_abstract_string not implemented" ;;
+
+let binop_to_abstract_string (bin: binop) : string =
+  match bin with
+  | Plus -> "Plus"
+  | Minus -> "Minus"
+  | Times -> "Times"
+  | Equals -> "Equls"
+  | LessThan -> "LessThan"
+  ;;
+
+let rec exp_to_abstract_string (exp : expr) : string =
+  match exp with
+  | Var x -> "Var(" ^ x ^ ")"
+  | Num x -> "Num(" ^ (string_of_int x) ^ ")"
+  | Bool x -> "Bool(" ^ (string_of_bool x) ^ ")"
+  | Unop (_, y) -> "Unop(" ^ "Negate, " ^ (exp_to_abstract_string y) ^ ")"
+  | Binop (x, y, z) -> "Binop(" ^ (binop_to_abstract_string x) ^ ", "
+                                ^ (exp_to_abstract_string y) ^ ", "
+                                ^ (exp_to_abstract_string z) ^ ")"
+  | Conditional (x, y, z) -> "Conditional(" ^ (exp_to_abstract_string x)  ^ ", " 
+                                            ^ (exp_to_abstract_string y) ^ ", " 
+                                            ^ (exp_to_abstract_string z) ^ ")"
+  | Fun (x, y) -> "Fun(" ^ x ^ ", " ^ (exp_to_abstract_string y) ^ ")"
+  | Let (x, y, z) -> "Let(" ^ x ^ ", " ^ (exp_to_abstract_string y) ^ ", "
+                                       ^ (exp_to_abstract_string z) ^ ")"
+  | Letrec (x, y, z) -> "Letrec(" ^ x ^ ", " ^ (exp_to_abstract_string y) ^ ", "
+                                       ^ (exp_to_abstract_string z) ^ ")"
+  | Raise -> raise Exit
+  | Unassigned -> "Unassigned"
+  | App (x, y) -> "App(" ^ (exp_to_abstract_string x) ^ ", " ^ (exp_to_abstract_string y) ^ ")" ;;
